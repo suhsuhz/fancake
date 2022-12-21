71260007889f13080853c822d2db07e6714af052
suhsuhz <31497805+suhsuhz@users.noreply.github.com>
2022-12-21 17:38:19 +0900

    Delete src directory

--- src/components/profile/CommonProfile.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="profile_container">
-        <router-view></router-view>
-    </div>
-</template>
-
-<style scoped>
-@import url('@/assets/css/profile/profile-common.css');
-</style>
